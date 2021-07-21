e4a7ec056a9947ed5bda733347ff83737cc26048
Jinok <joseo46@hotmail.com>
2021-07-21 20:39:29 +1000

    add cancel button

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -3,27 +3,69 @@ import './ExpenseForm.css';
 
 function ExpenseForm(props) {
   const [enteredTilte, setEnteredTilte] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const titleChangeHandler = (event) => {
+    setEnteredTilte(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTilte,
+      amount: +enteredAmount,
+      date: new Date(enteredDate),
+    };
+  };
+
   return (
     <div className='container'>
-      <form className='form'>
+      <form className='form' onSubmit={submitHandler}>
         <div className='new-expense__controls'>
           <div className='new-expense__control'>
             <label>Title</label>
-            <input type='text' />
+            <input
+              value={enteredTilte}
+              type='text'
+              onChange={titleChangeHandler}
+            />
           </div>
 
           <div className='new-expense__control'>
             <label>Amount</label>
-            <input type='number' />
+            <input
+              type='number'
+              value={enteredAmount}
+              onChange={amountChangeHandler}
+              min='0.01'
+              step='0.01'
+            />
           </div>
 
           <div className='new-expense__control'>
             <label>Date</label>
-            <input type='date' />
+            <input
+              type='date'
+              onChange={dateChangeHandler}
+              vlaue={enteredDate}
+              min='01-01-2018'
+              max='31-12-2023'
+            />
           </div>
 
           <div className='new-expense__actions'>
-            <button type='button'>Cancel</button>
+            <button type='button' onClick={props.onCancel}>
+              Cancel
+            </button>
             <button type='submit'>Add Expense</button>
           </div>
         </div>
--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 
-function NewExpense() {
+function NewExpense(props) {
+  const [startEditing, setStartEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setStartEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setStartEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setStartEditing(false);
+  };
+
   return (
-    <div>
-      <ExpenseForm />
+    <div className='new-expense'>
+      {!startEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {startEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 }
